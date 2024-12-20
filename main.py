4324bf8f2c6dfadaffe88ed610ebd1f7d7d7e306
lukapantar-personal <luka.pantar@gmail.com>
2024-12-20 16:33:01 +0100

    Removed access to the database, reworked so image scaling so it uses api calls

--- main.py
+++ main.py
@@ -7,21 +7,11 @@ import uuid
 import requests
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from database import SessionDependency, create_db_and_tables
 from enum import Enum
 from tempfile import SpooledTemporaryFile
 import os
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Creating database and tables...")
-    create_db_and_tables()
-
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+import json
+import io
 
 
 class ImageFormat(Enum):
@@ -66,12 +56,11 @@ class InternalImageProcessingJob(BaseModel):
     # This represents an image file path or S3 object name,
     # depending on which storage backend is configured.
     image_path: str
-    image_id: uuid.UUID
 
     # These are the actual processing parameters.
     resize_image_to_width: int
     resize_image_to_height: int
-    change_to_format: ImageFormat
+    change_to_format: str
 
 
 class PublicImageProcessingJobUpdateRequest(BaseModel):
@@ -87,76 +76,119 @@ class ProcessImageJob:
         self._zmq_context = zmq.Context()
 
         self._zmq_socket = self._zmq_context.socket(zmq.REP)
-        self._zmq_socket.connect(f"tcp://{settings.zmq_host}:{settings.zmq_port}")
-
-        while True:
-            try:
-                received_job_json = self._zmq_socket.recv()
-                job_conformation = InternalImageProcessingJobConfirmation(is_ok=True)
-            except Exception as err:
-                job_conformation = InternalImageProcessingJobConfirmation(is_ok=False)
-                raise
-            finally:
-                self._zmq_socket.send(job_conformation.model_dump_json())
-
-            received_job = InternalImageProcessingJob.model_validate_json(
-                received_job_json
-            )
-
-            self.transform_image(
-                received_job.image_path,
-                received_job.image_path,
-                received_job.resize_image_to_width,
-                received_job.resize_image_to_height,
-                received_job.change_to_format,
-            )
-
-            patch_obj = PublicImageProcessingJobUpdateRequest(
-                destination_image_id=uuid.uuid4,
-                status=ImageProcessingJobStatus.SUCCESS,
-            )
-
-            host = settings.photo_storage_host
-            port = settings.photo_storage_port
-            image_id = received_job.image_id
-            job_id = received_job.job_id
-
-            patch_url = f"http://{host}:{port}/images/{image_id}/jobs/{job_id}"
-
-            patch_response = requests.patch(
-                patch_url, json=patch_obj.model_validate_json()
-            )
-            if patch_response.status_code != 200:
-                print("failure")
+        self._zmq_socket.bind(f"tcp://{settings.zmq_host}:{settings.zmq_port}")
 
     def transform_image(
+        self,
+        image_buffer,
+        output_image_buffer,
         img_fpath: str,
-        new_img_fpath: str,
         resize_width: int,
         resize_height: int,
-        new_img_format: ImageFormat,
+        new_img_format: str,
     ):
+        # ) -> SpooledTemporaryFile:
         try:
-            storage = SessionDependency()
-            intermediate_image_buffer = SpooledTemporaryFile(mode="w+b")
-            storage.download_file(img_fpath, intermediate_image_buffer)
-            intermediate_image_buffer.seek(0, os.SEEK_SET)
+            # storage = SessionDependency()
+            # intermediate_image_buffer = SpooledTemporaryFile(mode="w+b")
+            # storage.download_file(img_fpath, intermediate_image_buffer)
+            # intermediate_image_buffer.seek(0, os.SEEK_SET)
 
-            with Image.open(intermediate_image_buffer) as img_obj:
+            with Image.open(image_buffer) as img_obj:
                 # old_img_format = img_obj.format
                 new_img_obj = img_obj.resize((resize_width, resize_height))
                 # new_img_obj = ImageOps.scale(img_obj, scale_factor, resample_method)
 
-                output_image_buffer = SpooledTemporaryFile(mode="w+b")
-                new_img_obj.save(output_image_buffer, new_img_format.value)
+                new_img_obj.save(output_image_buffer, new_img_format)
+                # new_img_obj.save("./testimage.jpeg", new_img_format)
                 output_image_buffer.seek(0, os.SEEK_SET)
 
-            storage.upload_file(new_img_fpath, output_image_buffer)
+            # storage.upload_file(img_fpath, output_image_buffer)
+            # return output_image_buffer
 
         except FileNotFoundError as err:
             raise
 
 
+process_image_job = ProcessImageJob()
+
 if __name__ == "__main__":
-    process_image_job = ProcessImageJob()
-#    uvicorn.run(users_app, host="0.0.0.0", port=8002)
+    # process_image_job = ProcessImageJob()
+
+    while True:
+        try:
+            received_job_bytes = process_image_job._zmq_socket.recv()
+            print("Received job.")
+            job_conformation = InternalImageProcessingJobConfirmation(is_ok=True)
+        except Exception as err:
+            job_conformation = InternalImageProcessingJobConfirmation(is_ok=False)
+            raise
+        finally:
+            process_image_job._zmq_socket.send(
+                (job_conformation.model_dump_json()).encode("utf-8")
+            )
+
+        received_job = InternalImageProcessingJob.model_validate_json(
+            received_job_bytes
+        )
+
+        host = settings.photo_storage_host
+        port = settings.photo_storage_port
+        job_id = received_job.job_id
+
+        download_specific_image_url = (
+            f"http://{host}:{port}/worker/jobs/{job_id}/download-source-image"
+        )
+        download_specific_image_get_response = requests.get(
+            download_specific_image_url, stream=True
+        )
+
+        if download_specific_image_get_response.status_code != 200:
+            raise Exception("Failed to download specific image.")
+
+        filename = (
+            download_specific_image_get_response.headers.get("Content-Disposition", "")
+            .split("filename=")[1]
+            .strip('"')
+        )
+
+        image_buffer = SpooledTemporaryFile(mode="wb")
+        image_buffer.write(download_specific_image_get_response.content)
+
+        output_image_buffer = SpooledTemporaryFile(mode="w+b")
+
+        process_image_job.transform_image(
+            image_buffer,
+            output_image_buffer,
+            received_job.image_path,
+            received_job.resize_image_to_width,
+            received_job.resize_image_to_height,
+            received_job.change_to_format,
+        )
+
+        upload_processes_image_url = (
+            f"http://{host}:{port}/worker/jobs/{job_id}/finalize"
+        )
+        # upload_image = {"filename": filename, "file": output_image_buffer}
+        upload_image = {"file": (filename, output_image_buffer)}
+        # with open(filename, "rb") as file_to_send:
+        # upload_image = {"file": file_to_send}
+        upload_processes_image_post_response = requests.post(
+            upload_processes_image_url, files=upload_image
+        )
+
+        if upload_processes_image_post_response.status_code != 200:
+            print(upload_processes_image_post_response.text)
+            raise Exception("Failed to upload processesed image.")
+
+        job_update_patch_obj = PublicImageProcessingJobUpdateRequest(
+            status=ImageProcessingJobStatus.SUCCESS.value,
+        )
+
+        job_update_patch_url = f"http://{host}:{port}/worker/jobs/{job_id}"
+
+        job_update_patch_response = requests.patch(
+            job_update_patch_url, json=job_update_patch_obj.model_validate_json()
+        )
+        if job_update_patch_response.status_code != 200:
+            raise Exception("Failed to send update_job_status to storage.")
